c7b9ffc06d0a730c3db20f7d31f03f272a8f8639
Rocco <roccorighi@hotmail.com>
2015-05-27 15:42:13 +0100

    adds color to background depending on temperature

--- public/js/src/button.js
+++ public/js/src/button.js
@@ -2,14 +2,33 @@ var thermostat = new Thermostat();
 
 $(document).ready( function() {
   $('#down').on('click', function(){
-  thermostat.lowerTemp();
+    thermostat.lowerTemp();
     $('#temperature').html(thermostat.degrees);
- });
-});
 
-$(document).ready( function() {
-  $('#up').on('click', function (){
+    if (thermostat.degrees < 18) {
+        $('html').removeClass("yellow");
+        $('html').addClass("green");
+    } else if (thermostat.degrees >= 18 && thermostat.degrees <= 25) {
+        $('html').removeClass("green");
+        $('html').removeClass("red");
+        $('html').addClass("yellow");
+    } else {
+        $('html').removeClass("yellow");
+        $('html').addClass("red");    }
+  });
+
+  $('#up').on('click', function(){
     thermostat.raiseTemp();
-      $('#temperature').html(thermostat.degrees);
+    $('#temperature').html(thermostat.degrees);
+    if (thermostat.degrees < 18) {
+        $('html').removeClass("yellow");
+        $('html').addClass("green");
+    } else if (thermostat.degrees >= 18 && thermostat.degrees <= 25) {
+        $('html').removeClass("green");
+        $('html').removeClass("red");
+        $('html').addClass("yellow");
+    } else {
+        $('html').removeClass("yellow");
+        $('html').addClass("red");    }
   });
 });
